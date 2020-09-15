e17543eff954e23688d01cc1d487fec8d45b7d88
huynhthetong280619 <huynhthetong1999999@gmail.com>
2020-09-15 16:09:22 +0700

    create page course and apply antd component

---
+++ pages/course/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout, Row } from 'antd'
+
+const PageCourse = () => {
+    return (
+        <Layout>
+            <Row>
+                <div>Hello world</div>
+            </Row>
+        </Layout>
+    )
+}
+
+export default PageCourse
